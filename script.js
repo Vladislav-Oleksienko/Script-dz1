7142619dc7fb96fa99dccfeb81e31fa290bf164d
Vladislav-Oleksienko <38045594+Vladislav-Oleksienko@users.noreply.github.com>
2018-04-04 15:58:50 +0300

    Script

---
+++ script.js
@@ -0,0 +1,27 @@
+/*var funcSumEl = function() {
+    var i;
+    var sum = 0;
+    for(i = 0; i < arguments.length; i++){
+      sum += arguments[i];
+    }
+    return sum;
+  }
+
+  console.log(funcSumEl(1,2,3,4));*/
+
+
+/*  function funcAs() {
+  var a = [4,1,5,9,2,3],
+      i ,b,c,n = a.length;
+  for (b = 0; b < n;b++){
+  for(i = 0; i < n-1; i++){
+    if(a[i] > a[i+1]){
+      c = a[i];
+      a[i] = a[i+1];
+      a[i+1] = c;
+    }
+  }
+  }
+  return a;
+  }
+  console.log(funcAs()); */
